fa6f44762a7c9b837c1fe443277f013643ed505a
pussbb <pussbb@gmail.com>
2012-09-27 09:07:26 +0300

    fix

---
+++ application/config/media.php
@@ -0,0 +1,27 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+return array(
+    'core' => array(
+        'static_uri' => 'uri',
+        'uri' => 'media/',
+        'path' => DOCROOT.'media'.DIRECTORY_SEPARATOR,
+    ),
+    'default' => array(
+        'css' => array(
+            'bootstrap/bootstrap.min' => '',
+            'bootstrap/bootstrap-responsive.min' => '',
+            'main' => '',
+        ),
+        'js' => array(
+            'jquery/jquery.min',
+            'bootstrap/bootstrap.min',
+            array(
+                'name' => 'ui_lib',
+                'files' => array(
+                    'lib/pseudo_ajax_load_progress',
+                    'lib/inline_alert',
+                ),
+            ),
+        ),
+    ),
+);
