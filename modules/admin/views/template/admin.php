95658176684edd2ac829e3ba7a02876e7eb11eb0
pussbb <pussbb@gmail.com>
2012-10-22 21:36:29 +0300

    admins area clean up

--- modules/admin/views/template/admin.php
+++ modules/admin/views/template/admin.php
@@ -2,60 +2,112 @@
 
     echo View::factory('template/header', get_defined_vars())->render();
 
-        echo '<div id="header">
-            <h1><a href="./dashboard.html"></a></h1>
-        </div>';
+        echo '<div id="header">';
+            echo '<h1>';
+            echo HTML::anchor('amdin', tr('Admin area'));
+            echo '</h1>';
+        echo '</div>';
         ?>
-        <div id="search">
-            <input type="text" placeholder="Search here..."/><button type="submit" class="tip-right" title="Search"><i class="icon-search icon-white"></i></button>
-        </div>
+<script>
+$(document).ready(function(){
+    $('.submenu > a').click(function(e)
+    {
+        e.preventDefault();
+        var submenu = $(this).siblings('ul');
+        var li = $(this).parents('li');
+        var submenus = $('#sidebar li.submenu ul');
+        var submenus_parents = $('#sidebar li.submenu');
+        if(li.hasClass('open'))
+        {
+            if(($(window).width() > 768) || ($(window).width() < 479)) {
+                submenu.slideUp();
+            } else {
+                submenu.fadeOut(250);
+            }
+            li.removeClass('open');
+        } else
+        {
+            if(($(window).width() > 768) || ($(window).width() < 479)) {
+                submenus.slideUp();
+                submenu.slideDown();
+            } else {
+                submenus.fadeOut(250);
+                submenu.fadeIn(250);
+            }
+            submenus_parents.removeClass('open');
+            li.addClass('open');
+        }
+    });
+
+    var ul = $('#sidebar > ul');
+
+    $('#sidebar > a').click(function(e)
+    {
+        e.preventDefault();
+        var sidebar = $('#sidebar');
+        if(sidebar.hasClass('open'))
+        {
+            sidebar.removeClass('open');
+            ul.slideUp(250);
+        } else
+        {
+            sidebar.addClass('open');
+            ul.slideDown(250);
+        }
+    });
+});
+</script>
         <div id="user-nav" class="navbar navbar-inverse">
-                    <ul class="nav btn-group">
+            <ul class="nav btn-group">
                 <li class="btn btn-inverse" ><a title="" href="#"><i class="icon icon-user"></i> <span class="text">Profile</span></a></li>
-                <li class="btn btn-inverse dropdown" id="menu-messages"><a href="#" data-toggle="dropdown" data-target="#menu-messages" class="dropdown-toggle"><i class="icon icon-envelope"></i> <span class="text">Messages</span> <span class="label label-important">5</span> <b class="caret"></b></a>
+                <li class="btn btn-inverse dropdown" id="menu-messages">
+                    <a href="#" data-toggle="dropdown" data-target="#menu-messages" class="dropdown-toggle"><span class="text"><?php echo tr('Tools');?></span> <b class="caret"></b></a>
                     <ul class="dropdown-menu">
-                        <li><a class="sAdd" title="" href="#">new message</a></li>
-                        <li><a class="sInbox" title="" href="#">inbox</a></li>
-                        <li><a class="sOutbox" title="" href="#">outbox</a></li>
-                        <li><a class="sTrash" title="" href="#">trash</a></li>
+                        <li><a title="" href="#"><?php echo tr('Languages');?></a></li>
+                        <li><a title="" href="#"><?php echo tr('Clean cache');?></a></li>
+                        <li><a title="" href="#"><?php echo tr('Logs');?></a></li>
+                        <li><a title="" href="#"><?php echo tr('Backup');?></a></li>
                     </ul>
                 </li>
-                <li class="btn btn-inverse"><a title="" href="#"><i class="icon icon-cog"></i> <span class="text">Settings</span></a></li>
-                <li class="btn btn-inverse"><a title="" href="login.html"><i class="icon icon-share-alt"></i> <span class="text">Logout</span></a></li>
+                <li class="btn btn-inverse">
+                  <a title="" href="#"><i class="icon icon-cog"></i> <span class="text"><?php echo tr('Settings');?></span></a>
+                </li>
+                <li class="btn btn-inverse">
+                    <?php
+                        echo HTML::anchor('users/logout',
+                            '<i class="icon icon-share-alt"></i> <span class="text">'.tr('Logout').'</span>'
+                        );
+                    ?>
+                </li>
             </ul>
          </div>
 
         <div id="sidebar">
-            <a href="#" class="visible-phone"><i class="icon icon-home"></i> Dashboard</a>
             <ul>
-                <li class="active"><a href="index.html"><i class="icon icon-home"></i> <span>Dashboard</span></a></li>
-                <li class="submenu">
-                    <a href="#"><i class="icon icon-th-list"></i> <span>Form elements</span> <span class="label">3</span></a>
-                    <ul>
-                        <li><a href="form-common.html">Common elements</a></li>
-                        <li><a href="form-validation.html">Validation</a></li>
-                        <li><a href="form-wizard.html">Wizard</a></li>
-                    </ul>
-                </li>
-                <li><a href="buttons.html"><i class="icon icon-tint"></i> <span>Buttons &amp; icons</span></a></li>
-                <li><a href="interface.html"><i class="icon icon-pencil"></i> <span>Interface elements</span></a></li>
-                <li><a href="tables.html"><i class="icon icon-th"></i> <span>Tables</span></a></li>
-                <li><a href="grid.html"><i class="icon icon-th-list"></i> <span>Grid Layout</span></a></li>
-                <li class="submenu">
-                    <a href="#"><i class="icon icon-file"></i> <span>Sample pages</span> <span class="label">4</span></a>
-                    <ul>
-                        <li><a href="invoice.html">Invoice</a></li>
-                        <li><a href="chat.html">Support chat</a></li>
-                        <li><a href="calendar.html">Calendar</a></li>
-                        <li><a href="gallery.html">Gallery</a></li>
-                    </ul>
-                </li>
-                <li>
-                    <a href="charts.html"><i class="icon icon-signal"></i> <span>Charts &amp; graphs</span></a>
-                </li>
-                <li>
-                    <a href="widgets.html"><i class="icon icon-inbox"></i> <span>Widgets</span></a>
-                </li>
+                <li class="active"><a href="#"><i class="icon icon-home"></i> <span>Dashboard</span></a></li>
+              <?php
+                foreach(Admin_Modules::instance()->modules() as $module) {
+                    $submenu = Arr::get($module, 'menu');
+                    echo '<li '.($submenu?'class="submenu"':'').'>';
+                    echo HTML::anchor('admin/'.Arr::path($module, 'info.uri'),
+                                        '<span>'.Arr::path($module, 'info.name').'</span>');
+                        
+                        if ($submenu) {
+                        ;
+                          echo '<ul>';
+                          foreach($submenu as $menu_item) {
+                              echo '<li>';
+                              echo HTML::anchor('admin/'.Arr::get($menu_item, 'uri'),
+                                        '<span>'.Arr::path($menu_item, 'title').'</span>');
+                              echo '</li>';
+                          }
+                          
+                          echo '</ul>';
+                        }
+                    echo '</li>';
+                }
+                
+              ?>
             </ul>
 
         </div>
@@ -63,24 +115,13 @@
         <?php
     echo '<div class="container" id="content">';?>
             <div id="content-header">
-                <h1>Dashboard</h1>
-                <div class="btn-group">
-                    <a class="btn btn-large tip-bottom" title="Manage Files"><i class="icon-file"></i></a>
-                    <a class="btn btn-large tip-bottom" title="Manage Users"><i class="icon-user"></i></a>
-                    <a class="btn btn-large tip-bottom" title="Manage Comments"><i class="icon-comment"></i><span class="label label-important">5</span></a>
-                    <a class="btn btn-large tip-bottom" title="Manage Orders"><i class="icon-shopping-cart"></i></a>
-                </div>
-            </div>
-            <div id="breadcrumb">
-                <a href="#" title="Go to Home" class="tip-bottom"><i class="icon-home"></i> Home ></a>
-                <a href="#" class="current">Dashboard</a>
-            </div>
-            <div class="container-fluid">
+
             </div>
 
+            <div class="container-fluid">
     <?php
       echo $content;
-    echo '</div>';
+    echo '</div></div>';
 
     echo View::factory('template/footer', get_defined_vars())->render();
 
